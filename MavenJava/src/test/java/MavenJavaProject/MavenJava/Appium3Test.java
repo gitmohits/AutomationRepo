8dab5b1b87ca943a92af036c8c981bc63aa73fd0
gitmohits <98281778+gitmohits@users.noreply.github.com>
2022-01-24 23:13:16 +0530

    Update from Github

--- MavenJava/src/test/java/MavenJavaProject/MavenJava/Appium3Test.java
+++ MavenJava/src/test/java/MavenJavaProject/MavenJava/Appium3Test.java
@@ -8,6 +8,6 @@ public class Appium3Test {
 	@Test
 	public void test3() {
 		System.out.println("test3");
-		System.out.println("This is the third push of");
+		System.out.println("This is the third push of the Github");
 	}
 }
