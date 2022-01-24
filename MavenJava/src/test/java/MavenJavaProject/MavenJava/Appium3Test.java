7670f274a133959410d3d58c7bbf5f0b71aeff55
gitmohits <98281778+gitmohits@users.noreply.github.com>
2022-01-25 00:24:18 +0530

    last push from master

--- MavenJava/src/test/java/MavenJavaProject/MavenJava/Appium3Test.java
+++ MavenJava/src/test/java/MavenJavaProject/MavenJava/Appium3Test.java
@@ -8,6 +8,6 @@ public class Appium3Test {
 	@Test
 	public void test3() {
 		System.out.println("test3");
-		System.out.println("This is the third push of the Github master");
+		System.out.println("This is the last push for testing");
 	}
 }
